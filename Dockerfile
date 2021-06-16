bde61c7dff78aca55a72a62b9f5b7a76501b42c8
root <root@ip-172-31-55-70.ec2.internal>
2021-06-16 15:40:43 +0000

    Firts_release_changes

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:alpine3.7
+COPY . /app
+WORKDIR /app
+RUN pip install flask 
+EXPOSE 5005
+ENTRYPOINT [ "python3" ]
+CMD [ "fls.py" ]
---
+++ fls.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/')                         ####@app.route(/) is decorator, basically routing the request..and next return the value for it.
+
+@app.route('/hello')
+
+def Helloworld():
+
+   return "Hello From Aetna!!We are healthcare company.!!! This is our First product Release"
+
+
+
+if __name__ == '__main__':
+
+   app.debug = True
+
+   app.run(host = '0.0.0.0', port = 5005)
